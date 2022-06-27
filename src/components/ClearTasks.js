6d233efedfa927ae26d4c6c263ec267f59688251
meekranjer <logicism81@gmail.com>
2022-06-27 14:50:14 +0800

    Update ClearTasks.js

--- src/components/ClearTasks.js
+++ src/components/ClearTasks.js
@@ -1,17 +1,28 @@
-import { Flex, Button } from '@chakra-ui/react';
+import { Flex, Button, useToast } from '@chakra-ui/react';
 import { useState } from 'react';
 import supabase from '../supabase';
 
 export default function ClearTasks() {
+  const toast = useToast();
   const [loading, setLoading] = useState(false);
 
-  async function handleClear() {
+  async function handleDelete() {
+    if (!window.confirm('Are you sure you want to clear all tasks?')) return;
+
     setLoading(true);
     const { data, error } = await supabase
       .from('todos')
       .delete()
       .not('text', 'eq', 'Do not delete me');
     setLoading(false);
+
+    toast({
+      title: error || 'Tasks cleared!',
+      position: 'top',
+      status: error ? 'error' : 'success',
+      duration: 2000,
+      isClosable: true,
+    });
   }
 
   return (
@@ -22,14 +33,12 @@ export default function ClearTasks() {
         h="45"
         color="gray.500"
         mt="10"
-        onClick={handleClear}
+        onClick={handleDelete}
         isLoading={loading}
-        loadingText="Clearing"
+        loadingText="Clearing tasks"
       >
         Clear Tasks
       </Button>
     </Flex>
   );
 }
-
-// https://supabase.com/docs/guides/api#safeguards-towards-accidental-deletes-and-updates
